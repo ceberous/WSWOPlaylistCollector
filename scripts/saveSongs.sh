ed5b810d63beca617713d950f24ae4bf65e2f634
ceberous <cerbus.collin@gmail.com>
2019-02-01 22:29:43 -0800

    16

--- scripts/saveSongs.sh
+++ scripts/saveSongs.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo "smembers 'WSWO.SONGS'" | redis-cli -n 3 > songs_base64.txt
 #perl -MMIME::Base64 -ne 'print decode_base64($_);print "\n"' songs_base64.txt > songs.txt
-perl -MMIME::Base64 -ne 'my $lines =  decode_base64($_); my @values = split( "---" , $lines ); foreach my $val (@values){print "$val";}print "\n";' songs_base64.txt > songs.txt
+perl -MMIME::Base64 -ne 'my $lines =  decode_base64($_); my @values = split( "---" , $lines ); foreach my $val (@values){print "$val ";}print "\n";' songs_base64.txt > songs.txt
