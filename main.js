b384701b27e065d8e87fe1db61155deebdc7bedb
ceberous <cerbus.collin@gmail.com>
2019-02-02 17:08:58 -0800

    19

--- main.js
+++ main.js
@@ -25,7 +25,6 @@ const wswo_url = "http://radio.securenetsystems.net/songdata/v5/index.cfm?statio
 function get_last_20_songs() {
 	return new Promise( async function( resolve , reject ) {
 		try {
-
 			let body = await MAKE_REQUEST( wswo_url );
 			try { var $ = cheerio.load( body ); }
 			catch( err ) { resolve( false ); return; }
@@ -33,7 +32,6 @@ function get_last_20_songs() {
 			let main_container = $( "#makeMeScrollable" );
 			let songs = $( main_container ).children();
 
-			//let latest = [];
 			for ( let i = 0; i < songs.length; ++i ) {
 				let track_container = $( songs[ i ] ).children();
 				let track_info = $( track_container[ 0 ] ).children().text();
@@ -49,7 +47,6 @@ function get_last_20_songs() {
 				if ( !unique || unique === "false" || unique === "0" ) {
 					console.log( "New Song" );
 					await MyRedis.setAdd( R_Key , db_id_b64 );
-					//MyOBJ_DB[ "self" ][ "songs" ][ db_id_b64 ] = result;
 					result.id = db_id_b64;
 					result.time_stamp = track_info[ 0 ];
 					result.search_string = track_info[ 1 ] + " " + track_info[ 2 ] + " " + track_info[ 3 ];
@@ -59,8 +56,6 @@ function get_last_20_songs() {
 					console.log( result );
 				}
 			}
-			//console.log( latest );
-			//MyOBJ_DB.save();
 			resolve();
 		}
 		catch( error ) { console.log( error ); reject( error ); }
@@ -69,11 +64,6 @@ function get_last_20_songs() {
 
 ( async ()=> {
 
-	// MyOBJ_DB = new JFODB( "wswo_playlist_archive_0" );
-	// if ( !MyOBJ_DB[ "self" ][ "songs" ] ) {
-	// 	MyOBJ_DB[ "self" ][ "songs" ] = {};
-	// 	MyOBJ_DB.save();
-	// }
 	MyRedis = new RMU( 3 );
 	await MyRedis.init();
 	await sleep( 1000 );
@@ -81,6 +71,19 @@ function get_last_20_songs() {
 	console.log( "WSWO Playlist Collector Restarted" );
 	get_last_20_songs();
 
+	// Live On the Air Schedule
+	// Sunday: Wackadoo Wax 1am-2am, Kel Crum 12-3pm, Steve Radcliffe 6-9pm, Sunday Night Music & Conversation with Tony Peters 9:30-10:00
+	// Monday: Tony Peters 10 am-2pm, Panama Jack 3-7pm, Tim Darcy 7-10pm (Last Shift Dec 3rd),  John King 7-10pm (Starting Dec 10, 2018)
+	// Tuesday: Dr. Rock 7-10am, Jordan Kelly 10am-2pm, Blazin Brad 2:00-6:00 pm, Shelly “Glad Girl” Wax Carnival 8-11pm
+	// Wednesday: Lightning Rod Flash 12-4pm,  Mike Reisz 4-7 pm, Gene Charles 7-10 pm
+	// Thursday: Jerry Halasz 10am-2pm, John Bennett  2-6 pm, Chuck Berry 6-9pm
+	// Friday: To be Determine now that Football is completed
+	// Saturday: Chris Poole 11am-1pm, Gary Quinn 2-6pm, Mike Shaver “The Record Saver” 9-12pm
+	// Also Big Band is On Saturday Morning , Not Sure What Time
+
+	// So We Need To Seperated out Different Playlists for each Host
+	// Somehow come up with conversion for 'Now Playing' and 'Played 8 minutes ago' etc.. timestamps
+	// In order to know which song goes where.
 	setInterval( function() {
 		console.log( "Getting Latest Songs" );
 		get_last_20_songs();
--- scripts/saveSongs.sh
+++ scripts/saveSongs.sh
@@ -3,3 +3,14 @@ echo "smembers 'WSWO.SONGS'" | redis-cli -n 3 > songs_base64.txt
 #perl -MMIME::Base64 -ne 'print decode_base64($_);print "\n"' songs_base64.txt > songs.txt
 perl -MMIME::Base64 -ne 'my $lines =  decode_base64($_); my @values = split( "---" , $lines ); foreach my $val (@values){print "$val ";}print "\n";' songs_base64.txt > songs.txt
 perl -MMIME::Base64 -ne 'my $lines =  decode_base64($_); my @values = split( "---" , $lines ); print @values[0]; print " "; print @values[1];print "\n";' songs_base64.txt > songs_simple.txt
+#perl -MMIME::Base64 -ne 'my $lines =  decode_base64($_); my @values = split( "---" , $lines ); @values[0] =~s/&|\.|\,|(\(Part.*?)\)|Pt/g; @values[1] =~s/&|\.|\,|(\(Part.*?)\)|Pt/g; print @values[0]; print " "; print @values[1];print "\n";' songs_base64.txt > songs_simple.txt
+
+# /&|\.|\,|(\(Part.*?)\)|Pt/g
+# Dueling Banjos Eric Weissberg & Steve Mandell
+# Six Three Four Five Seven Eight Nine Wilson Pickett
+# Once Upon A Time Marvin Gaye & Mary Wells
+# Lookin' For Boys Pin Ups
+# The Sun Ain't Gonna Shine (Anymore) Walker Brothers
+# The Bertha Butt Boogie, Pt. 1 Jimmy Castor Bunch
+# Moon Walk (Part 1) Joe Simon
+# Bluebird Paul McCartney & Wings
